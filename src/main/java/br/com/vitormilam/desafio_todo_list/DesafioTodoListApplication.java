3d976d3545c408ab2c7bb80bc0755b48da058813
vitormilam <vijmilam@gmail.com>
2025-10-06 17:08:58 -0300

    Criando o projeto e sua estrutura em camadas

---
+++ src/main/java/br/com/vitormilam/desafio_todo_list/DesafioTodoListApplication.java
@@ -0,0 +1,13 @@
+package br.com.vitormilam.desafio_todo_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTodoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTodoListApplication.class, args);
+	}
+
+}
